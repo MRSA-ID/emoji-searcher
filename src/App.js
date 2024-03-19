5519e7d0cac842caa303f9ea7b34037e5fd23484
MRSA <sanggisda@gmail.com>
2024-03-19 14:34:02 +0700

    add: store response data to state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import axios from 'axios';
 
 function App() {
   const [emojisData, setEmojisData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
   
   useEffect(() => {
     async function fetchEmojis() {
-      const res = await axios.get('https://run.mocky.io/v3/5a982f64-218d-45d7-a380-ebe924d55631')
-      console.table(res)
+      setLoading(true)
+      try {
+        const res = await axios.get('https://run.mocky.io/v3/5a982f64-218d-45d7-a380-ebe924d55631')
+        setEmojisData(res.data)
+        
+        setLoading(false)
+        
+      } catch (error) {
+        console.error(error)
+        
+        setError(true)
+        setLoading(false)
+      }
     }
     fetchEmojis()
   }, [])
   
+  console.log('loading ? ',loading)
+  console.log('error ? ',error)
+  console.log('emojiData ? ',emojisData)
   
   return (
     <div>
