dea308e0fa22c003ec4c6bb9bbc70367fe0bdbd1
MRSA <sanggisda@gmail.com>
2024-03-20 14:02:35 +0700

    update: improve dynamic rendering emojis component

--- src/components/Emojis/index.jsx
+++ src/components/Emojis/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styles from './Emojis.module.css'
 import EmojiBox from '../EmojiBox'
+import Empty from '../Empty'
 
 import {filterEmojis} from '../../utils/filterEmojis'
 
@@ -17,18 +18,25 @@ const Emojis = ({ emojisData, searchText }) => {
 
   }, [emojisData, searchText])
   
+  if (filteredEmojis.length > 0) {
+    return (
+      <div className={styles.emojisGrid}>
+        {filteredEmojis.map((data, index) => (
+          <EmojiBox 
+            key={index}
+            title={data.title} 
+            symbol={data.symbol} 
+          />
+        ))}
+      </div>
+    )
+  } else {
+    return (
+      <Empty text="Oops, zero finding. Let's try another keyword!" />
+    )
+  }
 
-  return (
-    <div className={styles.emojisGrid}>
-      {filteredEmojis.map((data, index) => (
-        <EmojiBox 
-          key={index}
-          title={data.title} 
-          symbol={data.symbol} 
-        />
-      ))}
-    </div>
-  )
+  return 
 }
 
 Emojis.propTypes = {
