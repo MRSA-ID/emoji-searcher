2db9827abf62ba5e41565762365bbcc43402d341
MRSA <sanggisda@gmail.com>
2024-03-20 13:34:30 +0700

    add: create filterEmojis Utils

---
+++ src/utils/filterEmojis.js
@@ -0,0 +1,17 @@
+export const filterEmojis = ({ emojisData, searchText = "", maxResult = 20 }) => {
+  const filterEmojis = emojisData.filter(emoji => {
+
+    if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
+      return true
+    }
+
+    if(emoji.keywords.includes(searchText.toLowerCase())){
+      return true
+    }
+
+    return false
+  })
+
+  return filterEmojis.splice(0, maxResult)
+
+}
